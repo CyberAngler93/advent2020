1070fac3f4e80d0c4cb89b4480c075543809f7b9
CyberAngler93 <mperry37@alaska.edu>
2020-12-07 11:37:30 -0900

    fix: refactor 7b input parse
    Make it a little more dry, do your parsing before you populate any arrays.
    also including timing, runs nice and quick at ~5ms

--- day7/b.py
+++ day7/b.py
@@ -1,35 +1,36 @@
+import time
 file = "input.txt"
 
 
-def count_bags(bag_name, list_bags):
+def count_bags(bag_name, all_bags):
     total = 1
-    for bags in list_bags:
+    for bags in all_bags:
         if bags[0] == bag_name:
             for bags_inside in bags[1:]:
                 if bags_inside == 'no other':
                     return 1
                 else:
-                    total += int(bags_inside[0]) * count_bags(bags_inside[2:], list_bags)
+                    total += int(bags_inside[0]) * count_bags(bags_inside[2:], all_bags)
     return total
 
 
 def main():
+    start = time.time()
     cleaned_bags = []
-    lines = []
     with open(file, "r") as f:
-        for line in f:
-            lines.append(str(line).rstrip())
+        for item in f:
+            item = item.replace(" contain ", ",")
+            item = item.replace(".", "")
+            item = item.replace(" bags", "")
+            item = item.replace(" bag", "")
+            item = item.replace(", ", ",")
+            item = item.rstrip()
+            item = item.split(",")
+            cleaned_bags.append(item)
         f.close()
-    for item in lines:
-        item = item.replace(" contain ", ",")
-        item = item.replace(".", "")
-        item = item.replace(" bags", "")
-        item = item.replace(" bag", "")
-        item = item.replace(", ", ",")
-        item = item.rstrip()
-        item = item.split(",")
-        cleaned_bags.append(item)
     print(count_bags('shiny gold', cleaned_bags) - 1)
+    end = time.time()
+    print(end - start)
 
 
 if __name__ == "__main__":
